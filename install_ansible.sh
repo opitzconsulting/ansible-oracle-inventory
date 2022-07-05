74af39afbc6490a428ccb5ac162767d092266cbd
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2022-07-05 18:21:49 +0000

    ansible-controller: Ansible is moved into a Docker Container, ansible-lint moved to 6.2.2

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
 #
-pip3 install --user ansible===2.9.26
-pip3 install --user "ansible-lint[yamllint]"
+
+
+sudo apt install -y python3-pip pre-commit
+
+# https://github.com/ansible/ansible-lint/issues/1795
+# pip3 install --user "rich>=10.0.0,<11.0.0"
+
+pip3 install --user "yamllint==1.26.3"
+pip3 install --user "ansible-lint==6.2.2"
 
 # git pre-commit setup
 pip3 install --user pre-commit
